4c60ca2bb1d089979c8d2d68081ba9c64ad21664
Vinh Timoon <vinhm281@gmail.com>
2025-11-11 21:39:02 +0700

    v29

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState<string>("Vinh");
+  const [age, setAge] = useState<number>(21);
+  const [person, setPerson] = useState({ name: 'Vinh', age: 21 });
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.text}>
-          Vinh
+          {name}
+        </Text>
+        <Text>
+          {person.age}
         </Text>
       </View>
       <Text>Hello World!</Text>
@@ -14,14 +22,16 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  text:{
+  text: {
     fontSize: 30,
     color: 'red'
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20
   },
 });
