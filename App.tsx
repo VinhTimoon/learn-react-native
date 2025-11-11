6432ad25996511023682e4b537a39bf0d94e96fa
Vinh Timoon <vinhm281@gmail.com>
2025-11-11 21:16:35 +0700

    v28

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>
+          Vinh
+        </Text>
+      </View>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text:{
+    fontSize: 30,
+    color: 'red'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
